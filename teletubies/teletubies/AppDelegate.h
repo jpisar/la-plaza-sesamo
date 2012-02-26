ebac45d9cb932847491abe80cbb9673bfb2f07ba
amqafrjs <amqafrjs@gmail.com>
2012-02-25 20:23:51 -0500

    primera comitiada

---
+++ teletubies/teletubies/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  teletubies
+//
+//  Created by amqafrjs on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ teletubies/teletubiesTests/teletubiesTests.h
@@ -0,0 +1,13 @@
+//
+//  teletubiesTests.h
+//  teletubiesTests
+//
+//  Created by amqafrjs on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface teletubiesTests : SenTestCase
+
+@end
